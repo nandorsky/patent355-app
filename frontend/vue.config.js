1f779eec12db4031dfe233944ddd316f77b6dabc
Nate Andorsky <nandorsky@gmail.com>
2023-10-25 16:14:00 -0400

    trying to get vue app working

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   transpileDependencies: true,
-  outputDir: '../staticfiles',
-  indexPath: '../templates/index.html',
+  publicPath: 'http://localhost:8080',
+  outputDir: '../frontend/dist',
+  indexPath: '../../patent355/templates/index.html',
   publicPath: '/',
+}
+
+configureWebpack: {
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true
+    }
+  }
 }
